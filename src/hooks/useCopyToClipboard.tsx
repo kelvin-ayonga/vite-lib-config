c206f08d96a44351e7890ccc3f5850e4090702ae
Isaiah kelvin Ayonga <kelvin.ayonga@pixeledge.io>
2026-03-05 13:49:16 +0300

    first commit

---
+++ src/hooks/useCopyToClipboard.tsx
@@ -0,0 +1,29 @@
+import { useState } from 'react';
+
+const useCopyToClipboard = () => {
+  const [error, setError] = useState<Error | null>(null);
+  const [isCopied, setIsCopied] = useState<boolean>(false);
+  const copyTextToClipboard = async (text: string) => {
+    return await navigator?.clipboard?.writeText(text);
+  };
+
+  const handleCopy = (text: string) => {
+    return copyTextToClipboard(text)
+      .then(() => {
+        setIsCopied(true);
+        alert('Text copied to clipboard!');
+        // Show success toast notification
+      })
+      .catch((err) => {
+        setError(err);
+        setIsCopied(false);
+        // Show error toast notification
+        alert('Failed to copy text to clipboard. Please try again.');
+        console.error(err);
+      });
+  };
+
+  return { isCopied, error, handleCopy, setIsCopied };
+};
+
+export default useCopyToClipboard;
